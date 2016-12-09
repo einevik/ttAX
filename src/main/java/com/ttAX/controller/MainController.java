2a2d905ceeabe4f28654d40835078389a81cf031
Viktor <einevic@gmail.com>
2016-12-09 13:07:24 +0400

    Add files via upload

---
+++ src/main/java/com/ttAX/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.ttAX.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+public class MainController {
+
+    @RequestMapping(value ="/welcome", method = RequestMethod.GET)
+    public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Invalid username or password!");
+        }
+        model.setViewName("index");
+        return model;
+
+    }
+
+}
---
+++ web/mainPage.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mozhaev.vv
+  Date: 09.12.2016
+  Time: 11:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
